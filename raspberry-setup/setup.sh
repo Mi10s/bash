3a6d825d72d75cac44d308e93396b7d5c035ab32
Milos Stojicevic <milos.keza@gmail.com>
2020-02-29 10:22:29 +0100

    Add Samba setup

--- raspberry-setup/setup.sh
+++ raspberry-setup/setup.sh
@@ -5,11 +5,11 @@ enable_ssh () {
 }
 
 setup_static_ip () {
-  echo 'interface wlan0
+  echo "interface wlan0
   static ip_address=192.168.0.3/24
   static routers=192.168.0.1
   static domain_name_servers=89.216.1.30
-  ' >> /etc/dhcpcd.conf
+  " >> /etc/dhcpcd.conf
 }
 
 setup_wifi () {
@@ -23,10 +23,25 @@ setup_wifi () {
  }" >> /etc/wpa_supplicant/wpa_supplicant.conf
 }
 
+setup_samba () {
+  apt-get install samba samba-common-bin
+
+  echo "[NetworkStorage]
+  path = /mnt/NetworkStorage
+  available = yes
+  valid users = pi
+  read only = no
+  browsable = yes
+  public = yes
+  writable = yes" >> /etc/samba/smb.conf
+
+  echo "/dev/sda1 /mnt/NetworkStorage auto nofail,uid=pi,gid=pi,umask=002,noatime 0 0" >> /etc/fstab
+}
+
 init () {
 
 if [[ $(id -u) -ne 0 ]];
-	then echo "Please run this script as root!"
+	then echo "Please run this script as root"
 	exit 1
 fi
 
@@ -37,6 +52,7 @@ fi
 
 setup_static_ip
 enable_ssh
+setup_samba
 }
 
 init
